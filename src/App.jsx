fef218ad7068ab2c9b60a21989c75d6e14da575c
DangTranTuanAnh <dangtrantuananh.2002@gmail.com>
2023-05-27 01:19:10 +0700

    Store user with redux toolkit

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,10 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './components/Home';
 import RegisterPage from './pages/register';
+import { useEffect } from "react";
+import { callFetchAccount } from "./services/api";
+import { useDispatch } from "react-redux";
+import { doGetAccountAction } from "./redux/account/accountSlice";
 
 
 const Layout = () => {
@@ -23,35 +27,50 @@ const Layout = () => {
   )
 }
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Layout />,
-    errorElement: <div>404 not found</div>,
-    children: [
-      { index: true, element: <Home /> },
-      {
-        path: "contact",
-        element: <ContactPage />,
-      },
-      {
-        path: "book",
-        element: <BookPage />,
-      },
-    ],
-
-  },
-  {
-    path: "/login",
-    element: <LoginPage />,
-  },
-  {
-    path: "/register",
-    element: <RegisterPage />,
-  },
-]);
 
 export default function App() {
+
+  const dispatch = useDispatch();
+
+  const getAccount = async () => {
+    const res = await callFetchAccount();
+    if (res && res.data) {
+      dispatch(doGetAccountAction(res.data))
+    }
+  }
+
+  useEffect(() => {
+    getAccount();
+  }, [])
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      errorElement: <div>404 not found</div>,
+      children: [
+        { index: true, element: <Home /> },
+        {
+          path: "contact",
+          element: <ContactPage />,
+        },
+        {
+          path: "book",
+          element: <BookPage />,
+        },
+      ],
+
+    },
+    {
+      path: "/login",
+      element: <LoginPage />,
+    },
+    {
+      path: "/register",
+      element: <RegisterPage />,
+    },
+  ]);
+
   return (
     <RouterProvider router={router} />
   )
--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -3,19 +3,24 @@ import { Link, useNavigate } from 'react-router-dom';
 import { callLogin } from '../../services/api';
 import './login.scss';
 import { useState } from 'react';
-
+import { useDispatch } from 'react-redux';
+import { doLoginAction } from '../../redux/account/accountSlice';
 
 const LoginPage = () => {
     const navigate = useNavigate();
     const [isSubmit, setIsSubmit] = useState(false);
 
+    const dispatch = useDispatch();
+
     const onFinish = async (values) => {
+
         const { username, password } = values;
         setIsSubmit(true);
         const res = await callLogin(username, password);
         setIsSubmit(false);
         if (res?.data) {
             localStorage.setItem('access_token', res.data.access_token);
+            dispatch(doLoginAction(res.data.user))
             message.success('Đăng nhập tài khoản thành công!');
             navigate('/')
         } else {
