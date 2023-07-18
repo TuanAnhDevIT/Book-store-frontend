bb58347924a7fe63b5475b3678f9ee924ea97ea1
DangTranTuanAnh <dangtrantuananh.2002@gmail.com>
2023-07-19 01:17:52 +0700

    read file excel

--- src/components/Admin/User/data/UserImport.jsx
+++ src/components/Admin/User/data/UserImport.jsx
@@ -1,11 +1,14 @@
 import { Modal, Table } from "antd";
 import { InboxOutlined } from '@ant-design/icons';
 import { message, Upload } from 'antd';
+import * as XLSX from 'xlsx';
+import { useState } from "react";
 
 const { Dragger } = Upload;
 
 const UserImport = (props) => {
     const { setOpenModalImport, openModalImport } = props;
+    const [dataExcel, setDataExcel] = useState([])
 
     // https://stackoverflow.com/questions/51514757/action-function-is-required-with-antd-u pload-control-but-i-dont-need-it
     const dumyRequest = ({ file, onSuccess }) => {
@@ -24,11 +27,31 @@ const UserImport = (props) => {
         // action: 'https://www.mocky.io/v2/5cc8019d300000980a055e76',
         customRequest: dumyRequest,
         onChange(info) {
+            console.log(">>> check info: ", info)
             const { status } = info.file;
             if (status !== 'uploading') {
                 console.log(info.file, info.fileList);
             }
             if (status === 'done') {
+                // https://stackoverflow.com/questions/66171804/importing-xlsx-and-parsing-to-json
+                if (info.fileList && info.fileList.length > 0) {
+                    const file = info.fileList[0].originFileObj;
+                    const reader = new FileReader();
+                    reader.readAsArrayBuffer(file);
+                    reader.onload = function (e) {
+                        const data = new Uint8Array(e.target.result);
+                        const workbook = XLSX.read(data, { type: 'array' });
+                        const sheet = workbook.Sheets[workbook.SheetNames[0]];
+                        // const json = XLSX.utils.sheet_to_json(sheet);
+                        const json = XLSX.utils.sheet_to_json(sheet, {
+                            header: ["fullName", "email", "phone"],
+                            range: 1 //skip header row
+                        });
+                        console.log(">>> check json: ", json)//test header
+
+                        if (json && json.length > 0) setDataExcel(json)
+                    }
+                }
                 message.success(`${info.file.name} file uploaded successfully.`);
             } else if (status === 'error') {
                 message.error(`${info.file.name} file upload failed.`);
@@ -65,6 +88,7 @@ const UserImport = (props) => {
                 </Dragger>
                 <div style={{ paddingTop: 20 }}>
                     <Table
+                        dataSource={dataExcel}
                         title={() => <span>Dữ liệu upload:</span>}
                         columns={[
                             { dataIndex: 'fullName', title: 'Tên hiển thị' },
